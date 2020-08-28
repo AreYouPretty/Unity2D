1054a996098eca32562023a89041c3e58f72af7f
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-08-28 19:16:44 +1200

    Переключение карты в двумерный режим и создание карт

---
+++ Assets/Scripts/Memory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private Sprite image;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
