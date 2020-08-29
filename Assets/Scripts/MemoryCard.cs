0b97cdf71c3bba02acc61b9ceb3c73e2f03fe3ff
Anastasia <61227061+AreYouPretty@users.noreply.github.com>
2020-08-29 17:26:53 +1200

    Открытие карты по щелчку

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Memory : MonoBehaviour
+public class MemoryCard : MonoBehaviour
 {
     [SerializeField] private GameObject cardBack;
-    [SerializeField] private Sprite image;
+    [SerializeField] private SceneController controller;
 
-    void Start()
+    private int _id;
+
+
+    public int id
     {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
         GetComponent<SpriteRenderer>().sprite = image;
     }
     public void OnMouseDown()
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+    void Start()
+    {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
+    }
+}
